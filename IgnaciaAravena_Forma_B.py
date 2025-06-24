81e1365aa087fbb1f54a843eb0b387844bfc7812
Ig.Aravena <ig.aravenag@duocuc.cl>
2025-06-24 18:38:42 -0400

    Add files via upload

---
+++ IgnaciaAravena_Forma_B.py
@@ -0,0 +1,11 @@
+
+
+
+
+while True:
+    print("Concierto de Trap con el 'Conejo Simpático'")
+    print("__ Menú principal __")
+    print("1.- Comprar entrada.")
+    print("2.- Consultar entrada.")
+    print("3.- Cancelar entrada.")
+    print("4.- Salir.")
